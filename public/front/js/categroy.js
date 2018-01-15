170b7d82ea83adefc0390e28e88f2672780c3e68
mjw <291871150@qq.com>
2018-01-15 16:52:40 +0800

    pc端分类完成

---
+++ public/front/js/categroy.js
@@ -0,0 +1,39 @@
+/**
+ * Created by 马靖文 on 2018/1/15.
+ */
+;(function(){
+
+  mui('.mui-scroll-wrapper').scroll({
+    deceleration: 0.0005,
+    indicators: false,
+  });
+  //一级分类查询
+  $.ajax({
+    type:'get',
+    url:'/category/queryTopCategory',
+    success:function(info){
+      //console.log(info)
+      $('.fristName').html(template('oneTpl',info))
+    }
+  })
+
+  //获取一级分类id
+  $('.mui-scroll').on('click' ,'.list', function (){
+    var id=$(this).data('id')
+    $.ajax({
+      type:'get',
+      url:'/category/querySecondCategory',
+      data:{id:id},
+      success:function(info){
+        console.log(info);
+        $('.brand').html(template('twoTpl',info))
+      }
+    })
+
+  })
+
+
+
+
+
+})();
